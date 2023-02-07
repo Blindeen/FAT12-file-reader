025087bfa49d99199654711526cb2821c9b93c2b
Blindeen <kuba.szmajda@gmail.com>
2023-02-07 19:17:59 +0100

    add exemplary code in main

--- main.c
+++ main.c
@@ -1,6 +1,47 @@
 #include <stdio.h>
 
+#include "file_reader.h"
+
 int main() {
-    printf("Hello, World!\n");
+    struct disk_t* disk = disk_open_from_file("fat12_volume.img");
+    if (disk == NULL)
+    {
+        printf("Image opening error");
+        return 1;
+    }
+
+    struct volume_t* volume = fat_open(disk, 0);
+    if (volume == NULL)
+    {
+        disk_close(disk);
+        printf("Image contains corrupted FAT table");
+        return 2;
+    }
+
+    struct file_t* file = file_open(volume, "example.txt");
+    if (file == NULL)
+    {
+        fat_close(volume);
+        disk_close(disk);
+        printf("File doesn't exist in root directory");
+        return 3;
+    }
+
+    char buffer[1000] = {0};
+    size_t read_bytes = file_read(buffer, 1, 999, file);
+    if(read_bytes < 999)
+    {
+        file_close(file);
+        fat_close(volume);
+        disk_close(disk);
+        printf("Data can't be fully read");
+        return 4;
+    }
+
+    printf("File contents:\n%s\n", buffer);
+
+    file_close(file);
+    fat_close(volume);
+    disk_close(disk);
     return 0;
 }
